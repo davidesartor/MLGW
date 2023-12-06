Fix noise scaling and add STFT computation

chirping_binary.py
@@ -42,7 +42,8 @@ class Parameters(NamedTuple):
 def amplitude(times: npt.NDArray[np.floating], parameters: Parameters) -> npt.NDArray[np.floating]:
     tau = (parameters.tcoal_s - times) * (parameters.tcoal_s > times)
     G_Mc_over_c3 = NEWTON_CONST_SIU * parameters.Mc_kg / SPEED_LIGHT_SIU**3
-    return (SPEED_LIGHT_SIU / parameters.r_m) * (G_Mc_over_c3) ** (5 / 4) * (5 / tau) ** (1 / 4)
+    amp =  (SPEED_LIGHT_SIU / parameters.r_m) * (G_Mc_over_c3) ** (5 / 4) * (5 / tau) ** (1 / 4)
+    return np.where(tau > 0, amp, 0)
 
 
 def phase(times: npt.NDArray[np.floating], parameters: Parameters) -> npt.NDArray[np.floating]:

dataset.py
@@ -1,29 +1,37 @@
 from dataclasses import dataclass
 import numpy as np
+import scipy
 import chirping_binary
 import noise
 
 
 @dataclass
 class DatasetChirping:
-    n_measurements: int
-    sampling_period: float
+    episode_duration_s: float
+    sample_rate_Hz: float
     n_sources: int = 1
-    examples_per_epoch: int = 1024
+    episodes_per_epoch: int = 1024
+    stft_window_size: int = 32
+
+    @property
+    def sample_period_s(self):
+        return 1 / self.sample_rate_Hz
 
     @property
     def times(self):
-        return np.arange(0, self.n_measurements * self.sampling_period, self.sampling_period)
-
-    @property
-    def time_range(self):
-        return (0, self.times[-1])
-
-    def __getitem__(self, index):
-        parameters = chirping_binary.Parameters.sample(time_range=self.time_range)
-        source_signal = chirping_binary.h_plus(times=self.times, parameters=parameters)
-        noise_signal = noise.generate(self.sampling_period, self.n_measurements, noise.LIGOL())
-        return (source_signal + noise_signal, parameters)
+        return np.arange(0, self.episode_duration_s, self.sample_period_s)
 
     def __len__(self):
-        return self.examples_per_epoch
+        return self.episodes_per_epoch
+
+    def __getitem__(self, index):
+        parameters = chirping_binary.Parameters.sample(time_range=(0, self.episode_duration_s))
+        source_signal = chirping_binary.h_plus(times=self.times, parameters=parameters)
+        noise_signal = noise.generate(self.sample_period_s, len(self.times), noise.LIGOL())
+        f, t, psd = scipy.signal.stft(
+            source_signal + noise_signal,
+            fs=self.sample_rate_Hz,
+            nperseg=self.stft_window_size,
+            noverlap=self.stft_window_size // 4,
+        )
+        return (np.abs(psd), np.array(parameters))